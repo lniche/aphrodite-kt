2fe2f9750193ad7ce7c78e95314abf6c99b0b64f
lcc <chengcheng.lu@windstoneservice.com>
2024-10-24 09:56:42 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     "developmentOnly"("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.mysql:mysql-connector-j")
+//    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
