35657629d39251adb47263f0e00e73c8f4703392
lcc <chengcheng.lu@windstoneservice.com>
2024-10-30 17:29:13 +0800

    update

--- src/main/kotlin/top/threshold/aphrodite/controller/v1/UserController.kt
+++ src/main/kotlin/top/threshold/aphrodite/controller/v1/UserController.kt
@@ -10,15 +10,12 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
 import lombok.Data
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import top.threshold.aphrodite.constant.CacheKey
 import top.threshold.aphrodite.controller.BaseController
 import top.threshold.aphrodite.entity.ResultKt
-import top.threshold.aphrodite.entity.vo.UserVO
 import top.threshold.aphrodite.repository.IUserRepository
 import top.threshold.aphrodite.utils.RedisUtil
 import java.time.OffsetDateTime
@@ -39,6 +36,34 @@ class UserController(
     val userRepository: IUserRepository,
     val redisUtil: RedisUtil
 ) : BaseController() {
+    @Data
+    class GetUserResp {
+        /**
+         * 昵称
+         */
+        var nickname: String? = null
+
+        /**
+         * 用户编号
+         */
+        var userNo: Long? = null
+
+        /**
+         * 用户编码
+         */
+        var userCode: String? = null
+
+        /**
+         * 电子邮件
+         */
+        var email: String? = null
+
+        /**
+         * 电话号码
+         */
+        var phone: String? = null
+    }
+
     @Operation(
         summary = "获取用户",
         description = "获取用户",
@@ -48,40 +73,34 @@ class UserController(
         Parameter(name = "userCode", description = "用户编号", `in` = ParameterIn.PATH)
     )
     @GetMapping("/{userCode}")
-    fun getUser(@PathVariable userCode: String): ResultKt<UserVO?> {
+    fun getUser(@PathVariable userCode: String): ResultKt<GetUserResp?> {
         val actualUserCode = if (StrUtil.isBlank(userCode)) {
             loginUid()
         } else {
             userCode
         }
         val redisKey = CacheKey.USER + actualUserCode
-        val userVO = redisUtil.getObj(redisKey, UserVO::class.java)
+        val getUserResp = redisUtil.getObj(redisKey, GetUserResp::class.java)
 
-        if (userVO == null) {
+        if (getUserResp == null) {
             val userDO = userRepository.getByCode(actualUserCode)
             userDO?.let {
                 redisUtil[redisKey, it] = 60
-                return ResultKt.success(UserVO().apply { BeanUtil.copyProperties(it, this) })
+                return ResultKt.success(GetUserResp().apply { BeanUtil.copyProperties(it, this) })
             }
         }
 
-        userVO?.apply {
+        getUserResp?.apply {
             email = DesensitizedUtil.email(email)
             phone = DesensitizedUtil.mobilePhone(phone)
         }
 
-        return ResultKt.success(userVO)
+        return ResultKt.success(getUserResp)
     }
 
     @Data
     @Schema(description = "更新用户信息的请求对象")
     open class UpdateUserReq {
-        /**
-         * 用户名
-         */
-        @field:Schema(description = "用户名", example = "john_doe", required = true)
-        var username: String? = null
-
         /**
          * 昵称
          */
@@ -109,25 +128,6 @@ class UserController(
         BeanUtil.copyProperties(updateUserReq, userDO, "userCode")
         userRepository.updateById(userDO)
         return ResultKt.success()
-    }
-
-    @Data
-    @Schema(description = "更新用户信息的请求对象")
-    class CreateUserReq : UpdateUserReq() {
-        /**
-         * 手机号
-         */
-        @field:NotBlank(message = "手机号不能为空")
-        @field:Schema(description = "用户手机号", example = "13800138000", required = true)
-        @Pattern(regexp = "^\\+?[1-9]\\d{1,14}\$", message = "手机号格式不正确")
-        var phone: String? = null
-
-        /**
-         * 验证码
-         */
-        @field:NotBlank(message = "验证码不能为空")
-        @field:Schema(description = "用户注册时的验证码", example = "1234", required = true)
-        var code: String? = null
     }
 
     @Operation(
--- src/main/kotlin/top/threshold/aphrodite/entity/dto/UserDTO.kt
+++ src/main/kotlin/top/threshold/aphrodite/entity/dto/UserDTO.kt
@@ -4,33 +4,5 @@ import lombok.Data
 
 @Data
 class UserDTO {
-    /**
-     * 用户名
-     */
-    var username: String? = null
 
-    /**
-     * 昵称
-     */
-    var nickname: String? = null
-
-    /**
-     * 用户编号
-     */
-    var userNo: Long? = null
-
-    /**
-     * 用户编码
-     */
-    var userCode: String? = null
-
-    /**
-     * 电子邮件
-     */
-    var email: String? = null
-
-    /**
-     * 电话号码
-     */
-    var phone: String? = null
 }
