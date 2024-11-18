08205848abeb95737d602a503fec5269578a4a93
lcc <chengcheng.lu@windstoneservice.com>
2024-11-18 17:48:23 +0800

    update

--- src/main/kotlin/top/threshold/aphrodite/AphroditeApplication.kt
+++ src/main/kotlin/top/threshold/aphrodite/AphroditeApplication.kt
@@ -2,10 +2,13 @@ package top.threshold.aphrodite
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class AphroditeApplication
 
 fun main(args: Array<String>) {
+    System.setProperty("user.timezone", "UTC")
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     runApplication<AphroditeApplication>(*args)
 }
